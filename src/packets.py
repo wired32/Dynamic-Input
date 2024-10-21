a1028f79c16887556ea1e8e103f1100bb7c21c39
alternative <75221290+Hagapes@users.noreply.github.com>
2024-10-21 11:29:13 -0300

    Added InputConfigsPacket
    - For input handling.

--- src/packets.py
+++ src/packets.py
@@ -3,3 +3,11 @@ class CompletionPacket:
         self.content = completion
         self.shade = shade
         self.bufferlenght = bufferLen
+
+class InputConfigsPacket:
+    def __init__(self, buffer = None, indent = None, end = None, allow_empty_input = None, key = None):
+        self.buffer = buffer
+        self.end = end
+        self.indent = indent
+        self.allow_empty_input = allow_empty_input
+        self.key = key
